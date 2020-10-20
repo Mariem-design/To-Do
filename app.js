9384a3efae4d35e845b3fea256ff1936772a052a
Marie M <bjurfalk@gmail.com>
2020-10-20 14:38:41 +0200

    klar

---
+++ app.js
@@ -0,0 +1,26 @@
+const addTodoform = document.querySelector("#addTodoForm");
+const addTodoinput = document.querySelector("addTodoinputfield");
+const todoList = document.querySelector("#todoList");
+
+//Set focus on text input
+addTodoinput.focus();
+
+//add event listener for submitting form.
+addTodoform.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTodo(addTodoinput.value);
+});
+
+//add todo list
+function addTodo(todoText) {
+  todoList.insertAdjacentHTML(
+    "beforeend",
+    `<li>${todoText}<button onclick="deleteTodo(this)">delete</button></li>`
+  );
+  addTodoForm.reset();
+  addTodoinput.focus();
+}
+function deleteTodo(element) {
+  element.parentElement.remove();
+  addTodoInput.focus();
+}
